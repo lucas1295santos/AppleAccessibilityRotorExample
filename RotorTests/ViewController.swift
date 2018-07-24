6eb88e35ba78ce1228a14e215f543490136c1a59
Lucas Santos <lucassantos@Lucass-MacBook-Pro.local>
2018-07-24 20:01:03 -0300

    Comentarios

--- RotorTests/ViewController.swift
+++ RotorTests/ViewController.swift
@@ -3,7 +3,7 @@
 //  RotorTests
 //
 //  Created by Lucas Santos on 23/07/18.
-//  Copyright © 2018 Lucas Santos. All rights reserved.
+//  Copyright © 2018 Lucas Santos (https://github.com/lucas1295santos). All rights reserved.
 //
 
 import UIKit
@@ -13,16 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var rotorPropertyValueLabel: UILabel!
     
     private var rotorPropertyValue: Int = 50
-    var customRotor: [UIAccessibilityCustomRotor]?
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setupCustonRotor()
+    }
+    
+    private func setupCustonRotor() {
+        
+        //Create a custor Rotor option, it has a name that will be read by voice over, and a action that is a
+        //action called when this rotor option is interacted with.
+        //The predicate gives you info about the state of this interaction
         let propertyRotorOption = UIAccessibilityCustomRotor.init(name: "Custon Property") { (predicate) -> UIAccessibilityCustomRotorItemResult? in
             
+            //Get the direction of the movement when this rotor option is enablade
             let forward = predicate.searchDirection == UIAccessibilityCustomRotorDirection.next
             
+            //You can do any kind of business logic processing here
             if forward {
                 self.rotorPropertyValue += 5
             } else {
@@ -31,19 +39,16 @@ class ViewController: UIViewController {
             
             self.rotorPropertyValueLabel.text = String(self.rotorPropertyValue)
             
-            
+            //Return the selection of voice over to the element rotorPropertyValueLabel
+            //Use this return to select the desired selection that fills the purpose of its logic
             return UIAccessibilityCustomRotorItemResult.init(targetElement: self.rotorPropertyValueLabel , targetRange: nil)
         }
         
+        //Add this rotor option to the iOS rotor, so the user can find it while in yor app
         self.accessibilityCustomRotors = [propertyRotorOption]
     }
     
     
-    
-//    @objc private func changeValueMethod() {
-//        self.rotorPropertyValue += 5
-//        self.rotorPropertyValueLabel.text = String(self.rotorPropertyValue)
-//    }
 
 }
 
